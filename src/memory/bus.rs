560c8137177ec43232a5e9376bb6d21eced91a27
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2026-09-10 05:02:09 -0300

    fix(bus): add cartridge RAM addresses to write

--- src/memory/bus.rs
+++ src/memory/bus.rs
@@ -38,9 +38,12 @@ impl MemoryBus {
     pub fn write(&mut self, address: u16, value: u8) {
         match address {
             CARTRIDGE_ROM_START..=CARTRIDGE_ROM_END => self.cartridge.write(address, value),
+
             VRAM_START..=VRAM_END => {
                 self.vram[(address - VRAM_START) as usize] = value;
             }
+
+            CARTRIDGE_RAM_START..=CARTRIDGE_RAM_END => self.cartridge.write(address, value),
 
             WRAM_START..=WRAM_END => {
                 self.wram[(address - WRAM_START) as usize] = value;
